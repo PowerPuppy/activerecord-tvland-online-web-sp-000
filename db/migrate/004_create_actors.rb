dbdd62a58698bd2206b2d507b51234ca07d39bd6
Chris Cheong <chrisxcheong@gmail.com>
2020-01-02 02:38:34 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,9 +1,8 @@
-class CreateActor < ActiveRecord::Migration[5.2]
+class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t|
-      t.string :last_name
       t.string :first_name
+      t.string :last_name
     end
   end
-
 end
